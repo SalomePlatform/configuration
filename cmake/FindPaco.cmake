533f576c42783a685d8f92f69228a71232298123
vsr <vsr@opencascade.com>
2020-04-15 20:31:47 +0300

    Copyright update 2020

--- cmake/FindPaco.cmake
+++ cmake/FindPaco.cmake
@@ -8,7 +8,7 @@
 # the detection mechanism. 
 #
 #############################################################
-# Copyright (C) 2007-2019  CEA/DEN, EDF R&D, OPEN CASCADE
+# Copyright (C) 2007-2020  CEA/DEN, EDF R&D, OPEN CASCADE
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
